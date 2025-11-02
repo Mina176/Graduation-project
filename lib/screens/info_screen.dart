fecfacea39ff9037cde046a17ad62b5ebbf647de
Mina Ayman <mtrmena176@gmail.com>
2025-11-02 22:31:47 +0200

    backup with qr

--- lib/screens/info_screen.dart
+++ lib/screens/info_screen.dart
@@ -1,10 +1,8 @@
-import 'dart:typed_data';
-import 'dart:ui' as ui;
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:graduation_project/screens/login_screen.dart';
 import 'package:graduation_project/services/storage_helper/storage_helper.dart';
-import 'package:image_gallery_saver/image_gallery_saver.dart';
-import 'package:permission_handler/permission_handler.dart';
+
 import 'package:pretty_qr_code/pretty_qr_code.dart';
 
 class InfoScreen extends StatefulWidget {
@@ -18,63 +16,24 @@ class InfoScreen extends StatefulWidget {
 class _InfoScreenState extends State<InfoScreen> {
   late QrImage qrImage;
 
+  String username = StorageHelper().loadName();
+  String password = StorageHelper().loadPasswordBase64()!;
+
+  Map<String, dynamic> get userData => {
+        "username": username,
+        "password": password,
+      };
+
+  String get jsonString => jsonEncode(userData);
+
   @override
   void initState() {
     super.initState();
-
-    final qrCode = QrCode.fromData(
-      data: StorageHelper().loadPasswordBase64()!,
+    QrCode qrCode = QrCode.fromData(
+      data: jsonString,
       errorCorrectLevel: QrErrorCorrectLevel.H,
     );
-
     qrImage = QrImage(qrCode);
-  }
-
-  Future<void> _saveQrCode() async {
-    try {
-      // --- 1. Request Storage Permission ---
-      var status = await Permission.storage.request();
-      if (!status.isGranted) {
-        _showSnackBar("Storage permission denied");
-        return;
-      }
-
-      // --- 2. Generate Image Bytes ---
-      final ByteData? byteData = await qrImage.toImageAsBytes(
-        size: 512,
-        format: ui.ImageByteFormat.png,
-        decoration: const PrettyQrDecoration(),
-      );
-
-      if (byteData == null) {
-        _showSnackBar("Failed to generate QR code image");
-        return;
-      }
-
-      final Uint8List pngBytes = byteData.buffer.asUint8List();
-
-      // --- 3. Save Image to Gallery ---
-      final result = await ImageGallerySaver.saveImage(
-        pngBytes,
-        quality: 100,
-        name: "qr_code_${DateTime.now().millisecondsSinceEpoch}",
-      );
-
-      if (result['isSuccess'] == true) {
-        _showSnackBar("QR Code saved to Gallery!");
-      } else {
-        _showSnackBar("Failed to save QR code: ${result['errorMessage']}");
-      }
-    } catch (e) {
-      _showSnackBar("An error occurred: $e");
-    }
-  }
-
-  /// Helper to show a SnackBar
-  void _showSnackBar(String message) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(message)),
-    );
   }
 
   @override
@@ -82,6 +41,7 @@ class _InfoScreenState extends State<InfoScreen> {
     return Scaffold(
       appBar: AppBar(),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         spacing: 16,
         children: [
           ListTile(
@@ -104,19 +64,18 @@ class _InfoScreenState extends State<InfoScreen> {
             ),
           ),
           SizedBox(height: 32),
-          Text(
-            'This is your Backup Qr Code, Keep it in a safe place',
+          SizedBox(
+            width: 250,
+            child: Text(
+              'This is your Backup Qr Code\nTake screenshot Keep it in a safe place',
+              textAlign: TextAlign.center,
+            ),
           ),
           SizedBox(
             height: 250,
             child: PrettyQrView(
               qrImage: qrImage,
             ),
-          ),
-          ElevatedButton.icon(
-            icon: const Icon(Icons.save_alt),
-            label: const Text("Save to Gallery"),
-            onPressed: _saveQrCode,
           ),
         ],
       ),
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:graduation_project/screens/root_home_screen.dart';
 import 'package:graduation_project/services/storage_helper/storage_helper.dart';
-import 'package:graduation_project/widgets/custom_btn.dart';
+import 'package:image/image.dart' as img;
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+import 'package:zxing2/qrcode.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -13,6 +18,8 @@ class _LoginScreenState extends State<LoginScreen> {
   final nameController = TextEditingController();
   final passwordContoller = TextEditingController();
 
+  bool _isLoading = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,61 +29,130 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              CustomTextField(
+              TextField(
                 controller: nameController,
-                hintText: 'Enter Name',
+                decoration: InputDecoration(
+                  hintText: 'Enter Name',
+                ),
               ),
               SizedBox(
                 height: 8,
               ),
-              CustomTextField(
-                  controller: passwordContoller,
+              TextField(
+                controller: passwordContoller,
+                obscureText: true,
+                decoration: InputDecoration(
                   hintText: 'Enter Password',
-                  obscureText: true),
+                ),
+              ),
               SizedBox(height: 8),
-              CustomBtn(
-                btnText: 'Login',
-                onTap: () {
-                  if (nameController.text.isEmpty ||
-                      passwordContoller.text.isEmpty) {
-                    return;
-                  }
-                  StorageHelper().saveName(nameController.text);
-                  StorageHelper().savePassword(passwordContoller.text);
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => RootHomeScreen(),
-                    ),
-                  );
-                },
-              )
+              ElevatedButton(
+                onPressed: _isLoading
+                    ? null
+                    : () {
+                        _performLogin(
+                            name: nameController.text,
+                            password: passwordContoller.text);
+                      },
+                child: const Text('Login'),
+              ),
+              ElevatedButton(
+                onPressed: _isLoading ? null : _loginWithQrCode,
+                child: const Text('Login with Qr Code'),
+              ),
             ],
           ),
         ),
       ),
     );
   }
+
+  void _performLogin({required String? name, required String? password}) {
+    if (name == null || password == null) return;
+
+    // Save the data
+    StorageHelper().saveName(name);
+    StorageHelper().savePassword(password);
+
+    // Navigate
+    if (!mounted) return; // Check if the widget is still in the tree
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const RootHomeScreen(),
+      ),
+    );
+  }
+
+  Future<void> _loginWithQrCode() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      final picker = ImagePicker();
+      final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+      if (pickedFile == null) {
+        if (mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('No image selected.')),
+          );
+        }
+        return;
+      }
+
+      try {
+        final result = readQrcode(pickedFile.path);
+      } catch (e) {
+        if (mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('no qr found')),
+          );
+        }
+        return;
+      }
+
+      final result = readQrcode(pickedFile.path);
+
+      final Map<String, dynamic> userData = jsonDecode(result);
+      final String? username = userData['username'];
+      final String? password = userData['password'];
+      _performLogin(name: username, password: password);
+    } catch (e) {
+      if (mounted) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text('An error occurred: $e')));
+      }
+    } finally {
+      if (mounted) {
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    }
+  }
 }
 
-class CustomTextField extends StatelessWidget {
-  const CustomTextField({
-    super.key,
-    required this.controller,
-    this.obscureText = false,
-    required this.hintText,
-  });
+String readQrcode(String path) {
+  try {
+    var image = img.decodeJpg(File(path).readAsBytesSync())!;
 
-  final TextEditingController controller;
-  final bool obscureText;
-  final String hintText;
+    LuminanceSource source = RGBLuminanceSource(
+        image.width,
+        image.height,
+        image
+            .convert(numChannels: 4)
+            .getBytes(order: img.ChannelOrder.abgr)
+            .buffer
+            .asInt32List());
+    var bitmap = BinaryBitmap(GlobalHistogramBinarizer(source));
 
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-        controller: controller,
-        decoration: InputDecoration(
-          hintText: hintText,
-        ));
+    var reader = QRCodeReader();
+    var result = reader.decode(bitmap);
+    print(result.text);
+    return result.text;
+  } catch (e) {
+    throw 'no qr found';
   }
 }
